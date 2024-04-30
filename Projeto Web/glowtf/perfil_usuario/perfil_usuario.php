b7c2c3a10eb3d2cee930d39f401078926feb643a
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-04-30 09:21:45 -0300

    Inicia design da página de perfil de usuário

--- Projeto Web/glowtf/perfil_usuario/perfil_usuario.php
+++ Projeto Web/glowtf/perfil_usuario/perfil_usuario.php
@@ -13,56 +13,6 @@ if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
-// Obtém os dados do formulário
-$name = $_POST['nome'];
-$lastname = $_POST['sobrenome'];
-$pass = $_POST['senha'];
-$email = $_POST['email'];
-$birthday_day = $_POST['dia'];
-$birthday_month = $_POST['mes'];
-$birthday_year = $_POST['ano'];
-
-if ($name != True) {
-    echo "Por favor, insira seu nome.\n";
-    return;
-}
-
-if ($lastname != True) {
-    echo "Por favor, insira seu sobrenome.\n";
-    return;
-}
-
-if ($pass != True) {
-    echo "Por favor, insira sua senha.\n";
-    return;
-}
-
-if ($email != True) {
-    echo "Por favor, insira seu email.\n";
-    return;
-}
-
-if (2024 - $birthday_year <= 18) {
-    echo "Usuário deve ter mais que 18 anos.\n";
-    return;
-}
-
-if (strlen($pass) <= 10) {
-    echo "Senha fraca, por favor insira 10 caracteres no mínimo.\n";
-    return;
-}
-
-// Insere os dados no banco de dados
-$sql = "INSERT INTO usuarios (nome, senha) VALUES ('$name', '$pass')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Dados inseridos com sucesso!\n";
-
-} else {
-    echo "Erro ao inserir dados: " . $conn->error;
-    
-}
-
 // Fecha a conexão
 $conn->close();
 ?>
