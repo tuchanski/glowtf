d4d36c570bd4474ebda1561ee7edee1d20ed094c
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-05-17 16:29:46 -0300

    Inicia implementação do PHP

--- Projeto Web/glowtf/admin/adicionar_produto/adicionar_produto.php
+++ Projeto Web/glowtf/admin/adicionar_produto/adicionar_produto.php
@@ -1,9 +1,9 @@
 <?php
 // Configurações do banco de dados
 $host = "localhost:3307";
-$usuario = "root";
+$usuario = "root"; 
 $senha = "";
-$banco = "bd_ex_cliente_servidor";
+$banco = "glowtfdb";
 
 // Conexão com o banco de dados
 $conn = new mysqli($host, $usuario, $senha, $banco);
@@ -13,6 +13,42 @@ if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
+// Obtém os dados do formulário
+$productName = $_POST['nome-produto'];
+$productPrice = $_POST['preco-produto'];
+$stock = $_POST['estoque'];
+$paint = $_POST['tinta'];
+// $productWiki = $_POST['wiki-produto'];
+$hatClass = $_POST['classe'];
+
+// Verifica se os inputs NOT NULL foram inseridos pelo usuário
+if ($productName != True) {
+    echo "Por favor, insira o nome do produto.\n";
+    return;
+}
+
+if ($productPrice != True) {
+    echo "Por favor, insira o preço do produto.\n";
+    return;
+}
+
+if ($stock != True) {
+    echo "Por favor, insira o estoque do produto.\n";
+    return;
+}
+
+// Insere os dados no banco de dados
+$sql = "INSERT INTO usuarios (nome, senha) VALUES ('$name', '$pass')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Dados inseridos com sucesso!\n";
+
+} else {
+    echo "Erro ao inserir novo produto." . $conn->error;
+    
+}
+ 
 // Fecha a conexão
 $conn->close();
 ?>
+ 
