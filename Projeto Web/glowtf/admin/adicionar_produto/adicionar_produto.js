3bd3e7ace63779c15a8d5ee60bed2a3cc9e5c319
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-05-18 09:13:19 -0300

    Altera JS

--- Projeto Web/glowtf/admin/adicionar_produto/adicionar_produto.js
+++ Projeto Web/glowtf/admin/adicionar_produto/adicionar_produto.js
@@ -5,21 +5,32 @@ function cadastraProduto() {
     const formData = new
     FormData(formulario);
 
-  
-    fetch('adicionar_produto.php', {
-        method: 'POST',
-        body: formData
-    })
-        .then(response => response.text())
-        .then(data => {
-            console.log(data); 
-    })
+    if (document.getElementById('nome-produto').value != '' &&
+    document.getElementById('preco-produto').value != '' &&
+    document.getElementById('estoque').value != '' &&
+    document.getElementById('descricao').value != '' &&
+    document.getElementById('classe').value != '') {
 
-    .catch(error => {
-        console.error('Erro:', error);
-        alert('Ocorreu um erro no envio do formulário.');
-    });
+        fetch('adicionar_produto.php', {
+            method: 'POST',
+            body: formData
+        })
+            
+            .then(response => response.text())
+            .then(data => {
+            console.log(data); // Mostra a resposta do servidor no console
+            alert(data); // Mostra uma mensagem com a resposta do servidor
+        })
+            .catch(error => {
+            console.error('Erro', error);
+            alert('Ocorreu um erro ao enviar os dados do formulário.');
+        });
+        
+    alert("Dados inseridos com sucesso!");
 
-    console.log("ok");
+} else {
+    alert("Por favor preencha todos os campos!");
+};
     // window.location.href = "../lista_de_produtos/lista_de_produtos.html";
 }
+
--- Projeto Web/glowtf/admin/adicionar_produto/adicionar_produto.php
+++ Projeto Web/glowtf/admin/adicionar_produto/adicionar_produto.php
@@ -20,7 +20,7 @@ $inventory = $_POST['estoque'];
 $paint = $_POST['tinta'];
 // $productImage = $_POST['imagem'];
 // $productWiki = $_POST['wiki-produto'];
-// $description = $_POST['description'];
+$description = $_POST['descricao'];
 $hatClass = $_POST['classe'];
 
 // Verifica se os inputs NOT NULL foram inseridos pelo usuário
@@ -33,14 +33,14 @@ if ($productPrice != True) {
     echo "Por favor, insira o preço do produto.\n";
     return;
 }
-
+ 
 if ($inventory != True) {
     echo "Por favor, insira o estoque do produto.\n";
     return;
 }
 
 // Insere os dados no banco de dados
-$sql = "INSERT INTO hat (inventory, price, promo_image, name, paint, description) VALUES ('$inventory', '$productPrice', 'dados/imagens/itens_do_jogo/soldier/soldier_helmet_large.png', '$productName', $paint, 'Um elegante chapéu fedora.')";
+$sql = "INSERT INTO hat (inventory, price, promo_image, name, paint, description) VALUES ('$inventory', '$productPrice', 'dados/imagens/itens_do_jogo/soldier/soldier_helmet_large.png', '$productName', $paint, '$description')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Produto inserido com sucesso!\n";
