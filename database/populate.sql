ef4b50d0ba164e9f90ca08256223a8c4ef819f28
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-05-19 22:00:56 -0300

    Continua implementação do JS e altera inserts das tintas no BD

--- database/populate.sql
+++ database/populate.sql
@@ -1,43 +1,39 @@
--- Insert data into user table (3 regular users and 1 admin)
 INSERT INTO user (name, gender, state, cpf, email, password, admin, id_steam) VALUES
 ('Alice Smith', 'Female', 'RS', '12345678901', 'alice@example.com', 'password123', FALSE, '12345678901234567'),
 ('Bob Johnson', 'Male', 'PR', '23456789012', 'bob@example.com', 'password456', FALSE, '23456789012345678'),
 ('Charlie Brown', 'Male', 'SC', '34567890123', 'charlie@example.com', 'password789', FALSE, '34567890123456789'),
 ('Admin User', 'Female', 'SP', '45678901234', 'admin@example.com', 'adminpass', TRUE, '45678901234567890');
 
-
--- Inserir dados na tabela de tintas
 INSERT INTO paint (name, promo_image, hex_color) VALUES
-('O Amargo Sabor de Derrota e Limão', 'path/to/image.png', 'A8A9AA'),
-('Uma Extraordinária Abundância de Matiz', 'path/to/image.png', '7D4071'),
-('Um Distinto Toque de Cinza', 'path/to/image.png', 'B0B3BE'),
-('Distorção Violeta Perversa', 'path/to/image.png', '38F3AB'),
-('Peculiarmente Agradável ao Paladar', 'path/to/image.png', 'A57545'),
-('Ausência de Amarelo', 'path/to/image.png', '787878'),
-('A Cor dos Sonhos', 'path/to/image.png', '39CCCC'),
-('Apenas Distração', 'path/to/image.png', '0F0F0F'),
-('Escória dos Deuses', 'path/to/image.png', 'FFF700'),
-('O Gentil Despertar da Primavera', 'path/to/image.png', 'FAD201'),
-('Laranja Bonita', 'path/to/image.png', 'FF8C00'),
-('Cor Incomum', 'path/to/image.png', 'C0C0C0'),
-('Notavelmente Distinto', 'path/to/image.png', 'A52A2A'),
-('O Primeiro Sinal de Primavera', 'path/to/image.png', '00FF00'),
-('O Negro da Meia-Noite Após uma Janta Especial', 'path/to/image.png', '000000'),
-('Um Delicado Balaço de Caramelo', 'path/to/image.png', 'EDC9AF'),
-('Comercializável Raro', 'path/to/image.png', 'FFD700'),
-('Grito de Guerra', 'path/to/image.png', 'C51F22'),
-('Um Distinto Toco de Água', 'path/to/image.png', '5ED4FF'),
-('Vermelho 40', 'path/to/image.png', 'FF0040'),
-('Rosto Pálido de Alguém', 'path/to/image.png', 'FFDAB9'),
-('Pergaminho do Carteiro', 'path/to/image.png', 'F0E68C'),
-('Espírito de Equipamento', 'path/to/image.png', 'A4A4A4'),
-('Forma da Cabeça de Cabelo', 'path/to/image.png', 'FF4500'),
-('Aroma de Destino', 'path/to/image.png', '40E0D0'),
-('Noites de Fortaleza Negra', 'path/to/image.png', '0A0A0A'),
-('Reino Marinho do Mercenário', 'path/to/image.png', '306EFF'),
-('Tons de Equinócio', 'path/to/image.png', '007FFF'),
-('Esquadrão dos Estames Estilhaçados', 'path/to/image.png', 'DA70D6');
-
+('O Amargo Sabor de Derrota e Limão', 'O_Amargo_Sabor_de_Derrota_e_Limão.png', 'A8A9AA'),
+('Uma Extraordinária Abundância de Matiz', 'Uma_Extraordinária_Abundância_de_Matiz.png', '7D4071'),
+('Um Distinto Toque de Cinza', 'Um_Distinto_Toque_de_Cinza.png', 'B0B3BE'),
+('Distorção Violeta Perversa', 'Distorção_Violeta_Perversa.png', '38F3AB'),
+('Peculiarmente Agradável ao Paladar', 'Peculiarmente_Agradável_ao_Paladar.png', 'A57545'),
+('Ausência de Amarelo', 'Ausência_de_Amarelo.png', '787878'),
+('A Cor dos Sonhos', 'A_Cor_dos_Sonhos.png', '39CCCC'),
+('Apenas Distração', 'Apenas_Distração.png', '0F0F0F'),
+('Escória dos Deuses', 'Escória_dos_Deuses.png', 'FFF700'),
+('O Gentil Despertar da Primavera', 'O_Gentil_Despertar_da_Primavera.png', 'FAD201'),
+('Laranja Bonita', 'Laranja_Bonita.png', 'FF8C00'),
+('Cor Incomum', 'Cor_Incomum.png', 'C0C0C0'),
+('Notavelmente Distinto', 'Notavelmente_Distinto.png', 'A52A2A'),
+('O Primeiro Sinal de Primavera', 'O_Primeiro_Sinal_de_Primavera.png', '00FF00'),
+('O Negro da Meia-Noite Após uma Janta Especial', 'O_Negro_da_Meia-Noite_Após_uma_Janta_Especial.png', '000000'),
+('Um Delicado Balaço de Caramelo', 'Um_Delicado_Balaço_de_Caramelo.png', 'EDC9AF'),
+('Comercializável Raro', 'Comercializável_Raro.png', 'FFD700'),
+('Grito de Guerra', 'Grito_de_Guerra.png', 'C51F22'),
+('Um Distinto Toco de Água', 'Um_Distinto_Toco_de_Água.png', '5ED4FF'),
+('Vermelho 40', 'Vermelho_40.png', 'FF0040'),
+('Rosto Pálido de Alguém', 'Rosto_Pálido_de_Alguém.png', 'FFDAB9'),
+('Pergaminho do Carteiro', 'Pergaminho_do_Carteiro.png', 'F0E68C'),
+('Espírito de Equipamento', 'Espírito_de_Equipamento.png', 'A4A4A4'),
+('Forma da Cabeça de Cabelo', 'Forma_da_Cabeça_de_Cabelo.png', 'FF4500'),
+('Aroma de Destino', 'Aroma_de_Destino.png', '40E0D0'),
+('Noites de Fortaleza Negra', 'Noites_de_Fortaleza_Negra.png', '0A0A0A'),
+('Reino Marinho do Mercenário', 'Reino_Marinho_do_Mercenário.png', '306EFF'),
+('Tons de Equinócio', 'Tons_de_Equinócio.png', '007FFF'),
+('Esquadrão dos Estames Estilhaçados', 'Esquadrão_dos_Estames_Estilhaçados.png', 'DA70D6');
 
 INSERT INTO hat_class (hat_id, scout, soldier, pyro, demoman, heavy, engineer, medic, sniper, spy) VALUES
 (1, 0, 1, 0, 0, 0, 0, 0, 0, 0), -- Fedora (Soldier)
@@ -58,9 +54,6 @@ INSERT INTO hat (inventory, price, promo_image, name, paint, description, wiki) 
 (60, 70, 'engineer_hat_large.png', 'Chapéu de Palha', null, 'Um chapéu de palha perfeito para o verão.', 'https://wiki.teamfortress.com/wiki/Straw_Hat'),
 (45, 110, 'spy_hat_large.png', 'Elmet Costureiro', null, 'Um capacete usado por costureiros de elite.', 'https://wiki.teamfortress.com/wiki/Costume_helmet'),
 (35, 130, 'demo_helmet_large.png', 'Capacete Medieval', null, 'Um capacete usado por cavaleiros medievais.', 'https://wiki.teamfortress.com/wiki/Medieval_Helmet');
-
-
-
 
 INSERT INTO coupons (expiration_date, discount, uses, start_date, code_name) VALUES
 ('2024-06-30', 20, 100, '2024-05-17', 'PAOTASTICO2024'),
