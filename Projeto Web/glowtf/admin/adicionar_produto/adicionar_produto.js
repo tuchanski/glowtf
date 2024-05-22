c2dae33c815973e22cdb343a0a12317f1a4b741e
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-05-22 18:18:39 -0300

    Adiciona splash da tinta, somente se tiver uma selecionada

--- Projeto Web/glowtf/admin/adicionar_produto/adicionar_produto.js
+++ Projeto Web/glowtf/admin/adicionar_produto/adicionar_produto.js
@@ -1,28 +1,45 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-  const inputUploadImagem = document.getElementById('upload-imagem');
-  const divImagens = document.querySelector('.imagens');
-  const imgProduto = document.querySelector('.card-imagem-produto');
-
-  divImagens.style.display = 'none';
-
-  inputUploadImagem.addEventListener('change', function() {
-    if (inputUploadImagem.files.length > 0) {
-      divImagens.style.display = 'block';
-      const arquivo = inputUploadImagem.files[0];
-      const nomeArquivo = arquivo.name;
-      console.log(`Arquivo selecionado: ${nomeArquivo}`);
-      
-      const leitor = new FileReader();
-      leitor.onload = function(e) {
-        imgProduto.src = e.target.result;
-      };
-      leitor.readAsDataURL(arquivo);
-    } else {
-      divImagens.style.display = 'none';
-    }
+    const inputUploadImagem = document.getElementById('upload-imagem');
+    const divImagens = document.querySelector('.imagens');
+    const imgProduto = document.querySelector('.card-imagem-produto');
+    const selectTinta = document.getElementById('tinta');
+    const imgSplash = document.querySelector('.card-splash');
+  
+    // Inicialmente esconder a div "imagens"
+    divImagens.style.display = 'none';
+    imgSplash.style.display = 'none';
+  
+    inputUploadImagem.addEventListener('change', function() {
+      // Se um arquivo foi selecionado, mostrar a div "imagens"
+      if (inputUploadImagem.files.length > 0) {
+        divImagens.style.display = 'block';
+        const arquivo = inputUploadImagem.files[0];
+        const nomeArquivo = arquivo.name;
+        console.log(`Arquivo selecionado: ${nomeArquivo}`);
+        
+        const leitor = new FileReader();
+        leitor.onload = function(e) {
+          imgProduto.src = e.target.result;
+        };
+        leitor.readAsDataURL(arquivo);
+      } else {
+        // Se nenhum arquivo foi selecionado, esconder a div "imagens"
+        divImagens.style.display = 'none';
+      }
+    });
+  
+    selectTinta.addEventListener('change', function() {
+      const tintaSelecionada = selectTinta.value;
+      if (tintaSelecionada !== "Nenhuma" && tintaSelecionada !== "") {
+        imgSplash.src = `../../dados/imagens/tintas/${tintaSelecionada.replace(/ /g, '_')}.png`;
+        imgSplash.style.display = 'block';
+        console.log(imgSplash.src);
+      } else {
+        imgSplash.style.display = 'none';
+      }
+    });
   });
-});
+  
 
 function cadastraProduto() {
     const nomeProduto = document.getElementById('nome-produto').value;
