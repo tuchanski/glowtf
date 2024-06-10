3a459c2100bcc2a772a278eff06f51254d0993d9
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-06-10 12:16:07 -0300

    Corrige arquivo dos usuarios

--- PjBL_Fase_3/control.sql
+++ PjBL_Fase_3/control.sql
@@ -1,39 +1,31 @@
 -- control.sql
 -- Arquivo contendo as instruções de segurança e controle de acesso
 
--- Verificar se o usuário já existe e, se sim, removê-lo
-DROP USER IF EXISTS 'novo_usuario'@'localhost';
+-- Cria os usuários
+CREATE USER 'giovanni'@'localhost' IDENTIFIED BY '12345';
+CREATE USER 'ana'@'localhost' IDENTIFIED BY '98765';
+CREATE USER 'guilherme'@'localhost' IDENTIFIED BY '98453';
+CREATE USER 'bot_tf'@'localhost' IDENTIFIED BY '32157';
+CREATE USER 'gustavo'@'localhost' IDENTIFIED BY 'supergusta';
+CREATE USER 'antonio'@'localhost' IDENTIFIED BY '32057';
 
--- Criar um novo usuário para acesso ao banco de dados
-CREATE USER 'novo_usuario'@'localhost' IDENTIFIED BY 'senha_segura';
+-- Cria os papéis
+CREATE ROLE IF NOT EXISTS 'administrador';
+CREATE ROLE IF NOT EXISTS 'estagiario';
+CREATE ROLE IF NOT EXISTS 'bot_da_steam';
+CREATE ROLE IF NOT EXISTS 'cliente';
 
--- Conceder permissões ao novo usuário
-GRANT SELECT, INSERT, UPDATE, DELETE ON glowtfdb.* TO 'novo_usuario'@'localhost';
+-- Atribuir permissões aos papéis
+GRANT * ON *.* TO 'administrador' WITH GRANT OPTION;
+GRANT SELECT, INSERT, UPDATE ON glowtfdb.hat, glowtfdb.user TO 'estagiario';
+GRANT INSERT, ON glowtfdb.sale TO 'bot_da_steam';
+GRANT SELECT, UPDATE, ON glowtfdb.hat TO 'bot_da_steam';
+GRANT SELECT, INSERT, UPDATE ON glowtfdb.user TO 'cliente';
+GRANT SELECT ON glowtfdb.hat, glowtfdb.hat_has_class, glowtfdb.sale, glowtfdb.paint, glowtfdb.hat_class TO 'cliente';
 
--- Aplicar alterações de permissões
-FLUSH PRIVILEGES;
+-- Atribui os papéis aos usuários
+GRANT 'administrador' TO 'ana'@'localhost';
+GRANT 'cliente' TO 'giovanni'@'localhost';
+GRANT 'estagiario' TO 'guilherme'@'localhost';
+GRANT 'bot_da_steam' TO 'bot_tf'@'localhost';
 
--- Criar uma nova role (papel) para gerenciar permissões de forma centralizada
-CREATE ROLE IF NOT EXISTS 'role_gerente';
-
--- Atribuir permissões à role criada
-GRANT SELECT, INSERT, UPDATE, DELETE ON glowtfdb.* TO 'role_gerente';
-
--- Atribuir a role ao novo usuário
-GRANT 'role_gerente' TO 'novo_usuario'@'localhost';
-
--- Implementar auditoria
--- Criar uma tabela para registrar logs de acesso e operações realizadas pelos usuários
-CREATE TABLE IF NOT EXISTS glowtfdb.audit_log (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user VARCHAR(50),
-    operation VARCHAR(100),
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Exemplo de trigger para registrar operações de inserção na tabela 'paint'
-DROP TRIGGER IF EXISTS log_insert_paint;
-CREATE TRIGGER log_insert_paint
-AFTER INSERT ON glowtfdb.paint
-FOR EACH ROW
-INSERT INTO glowtfdb.audit_log (user, operation) VALUES (USER(), 'INSERT INTO paint');
