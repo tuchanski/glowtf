27af00318b49de1061e0f7268f0f93b881d0428c
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-06-11 09:38:58 -0300

    Inicia correção CSS

--- Projeto Web/glowtf/carrinho/carrinho.js
+++ Projeto Web/glowtf/carrinho/carrinho.js
@@ -29,30 +29,30 @@ function carregarProdutos(query) {
       console.log(data);
       data.forEach((data) => {
         const item = `<tr>
-          <td class="imagens">
+          <td class="imagens-carrinho">
           <a href="../produto/produto.html?hat_id=${data.hat_id}" class="imagens">
-            <img class="card-imagem-produto" src="../dados/imagens/itens_do_jogo/${data.hat_promo_image}">
-            <img class="card-splash" src="../dados/imagens/tintas/${data.paint_promo_image}">
+            <img class="imagem-produto-carrinho" src="../dados/imagens/itens_do_jogo/${data.hat_promo_image}">
+            <img class="splash-carrinho" src="../dados/imagens/tintas/${data.paint_promo_image}">
           </a>
           </td>
-          <td class="dados-produto">
-            <div class="dados-produto">
-            <div class="card-titulo">${data.hat_name}</div>
-                <div class="tinta">
-                <div class="cor-da-tinta" style="background-color: #${data.hex_color};"></div>
-                  <div class="nome-tinta">${data.paint_name}</div>
+          <td class="dados-produto-carrino">
+            <div class="dados-produto-carrinho">
+            <div class="titulo-carrinho">${data.hat_name}</div>
+                <div class="tinta-carrinho">
+                <div class="cor-da-tinta-carrinho" style="background-color: #${data.hex_color};"></div>
+                  <div class="nome-tinta-carrinho">${data.paint_name}</div>
                 </div>
             </div>
           </td>
-          <td class="preco">
+          <td class="preco-carrinho">
               R$ ${(data.price / 100).toFixed(2).replace('.', ',')}
           </td>
-          <td class="btn-comprar">
+          <td class="btn-comprar-carrinho">
             <button class="comprar-btn" type="button">
               Comprar
             </button>
           </td>
-          <td class="btn-deletar">
+          <td class="btn-deletar-carrinho">
             <button class="remover-carrinho-btn" type="button">
               <span class="material-symbols-outlined">
                 delete
