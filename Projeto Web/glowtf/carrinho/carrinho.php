e07f1335bcf50c2868e3176374eb16bf735071f6
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-06-11 14:52:07 -0300

    Atualiza query sql

--- Projeto Web/glowtf/carrinho/carrinho.php
+++ Projeto Web/glowtf/carrinho/carrinho.php
@@ -10,44 +10,43 @@ $conn = new mysqli($host, $usuario, $senha, $banco, $port);
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 } else {
-    $sql = "SELECT
-                hat.id AS hat_id,
-                hat.name AS hat_name,
-                hat.inventory,
-                hat.price,
-                hat.promo_image AS hat_promo_image,
-                hat.description,
-                hat.wiki,
-                paint.paint_id,
-                paint.name AS paint_name,
-                paint.promo_image AS paint_promo_image,
-                paint.hex_color
-            FROM
-                hat
-            LEFT JOIN
-                paint ON hat.paint_id = paint.paint_id
-            WHERE
-                hat.inventory > 0;";
-
     // $sql = "SELECT
-    //             h.name AS nome_produto,
-    //             c.id_user,
-    //             chh.id_hat,
-    //             h.price,
-    //             h.promo_image,
-    //             IF(h.name = 'hat', 'Yes', 'No') AS cart_has_hat
+    //             hat.id AS hat_id,
+    //             hat.name AS hat_name,
+    //             hat.inventory,
+    //             hat.price,
+    //             hat.promo_image AS hat_promo_image,
+    //             hat.description,
+    //             hat.wiki,
+    //             paint.paint_id,
+    //             paint.name AS paint_name,
+    //             paint.promo_image AS paint_promo_image,
+    //             paint.hex_color
     //         FROM
-    //             cart c
-    //         JOIN
-    //             cart_has_hat chh ON c.id_user = chh.id_cart
-    //         JOIN
-    //             hat h ON chh.id_hat = h.id
+    //             hat
+    //         LEFT JOIN
+    //             paint ON hat.paint_id = paint.paint_id
     //         WHERE
-    //             c.id_user = 2
-    //         GROUP BY
-    //             h.name, c.id_user, chh.id_hat, h.price, h.promo_image
-    //         ORDER BY
-    //             h.name;";
+    //             hat.inventory > 0;";
+
+    $sql = "SELECT
+                chh.id AS cart_has_hat_id,
+                chh.id_cart AS user_id,
+                chh.id_hat,
+                h.name AS hat_name,
+                h.price,
+                h.promo_image AS hat_promo_image,
+                p.name AS paint_name,
+                p.promo_image AS paint_promo_image,
+                p.hex_color AS paint_hex_color
+            FROM
+                cart_has_hat chh
+            JOIN
+                hat h ON chh.id_hat = h.id
+            LEFT JOIN
+                paint p ON h.paint_id = p.paint_id
+            WHERE
+                chh.id_cart = 4;";
 
     $result= $conn->query($sql);
    
