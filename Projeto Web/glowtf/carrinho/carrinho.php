b156ddd567fe0d648ccb50d075422742b87887bd
Tuchanski <gui.rocha2004@gmail.com>
2024-06-12 19:34:21 -0300

    Inserido botão delete funcional.

--- Projeto Web/glowtf/carrinho/carrinho.php
+++ Projeto Web/glowtf/carrinho/carrinho.php
@@ -10,25 +10,7 @@ $conn = new mysqli($host, $usuario, $senha, $banco, $port);
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 } else {
-    // $sql = "SELECT
-    //             hat.id AS hat_id,
-    //             hat.name AS hat_name,
-    //             hat.inventory,
-    //             hat.price,
-    //             hat.promo_image AS hat_promo_image,
-    //             hat.description,
-    //             hat.wiki,
-    //             paint.paint_id,
-    //             paint.name AS paint_name,
-    //             paint.promo_image AS paint_promo_image,
-    //             paint.hex_color
-    //         FROM
-    //             hat
-    //         LEFT JOIN
-    //             paint ON hat.paint_id = paint.paint_id
-    //         WHERE
-    //             hat.inventory > 0;";
-
+    
     $user = $_POST["id"];
     $sql = "SELECT
                 chh.id AS cart_has_hat_id,
