ce7b187f95eb9b0d0d7385eb76f8977d8c488989
Tuchanski <gui.rocha2004@gmail.com>
2024-06-12 13:17:33 -0300

    Atualiza PHP, Js e HTML condizente com outras pastas.

--- Projeto Web/glowtf/perfil_usuario/perfil_usuario.js
+++ Projeto Web/glowtf/perfil_usuario/perfil_usuario.js
@@ -1,43 +1,64 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtenha o ID do usuário da URL
-    const params = new URLSearchParams(window.location.search);
-    const userId = params.get("id_user");
+function carregaProdutos() {
+  const listaProdutos = document.getElementsByClassName("itens")[0];
+  fetch("perfil_usuario.php")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro na conexão");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      data.forEach((data) => {
+        const item = `
+        <tr>
+            <td class="imagens">
+                <img class="imagem" src="../dados/imagens/itens_do_jogo/${data.hat_promo_image}">
+                <img class="splash" src="../dados/imagens/tintas/${data.paint_promo_image}">
+            </td>
+            <td>
+              <div class="dados-produto">
+                <div class="titulo">${data.hat_name}</div>
+                  <div class="tinta">
+                    <div class="cor-da-tinta"></div>
+                    <div class="nome-tinta">${data.paint_name}</div>
+                  </div>
+              </div>
+            </td>
+            <td class="preco">
+              ${data.sale_date}
+          </td>
+            <td class="preco">
+            R$ ${data.sale_price}
+            </td>
+          </tr>
+        `
+        listaProdutos.insertAdjacentHTML("beforeend", item);
+      });
+    })
+    .catch((error) => console.error("Error:", error));
+}
+document.addEventListener("DOMContentLoaded", () => carregaProdutos(""));
 
-    if (userId) {
-        fetch(`perfil_usuario.php?id_user=${userId}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Erro na rede');
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.error) {
-                    document.getElementById("user-data").innerHTML = `Erro: ${data.error}`;
-                } else {
-                    if (data.length > 0) {
-                        let htmlContent = "<table border='1'>";
-                        htmlContent += "<tr><th>Data</th><th>Preço da Venda</th><th>Nome do Chapéu</th><th>Descrição do Chapéu</th></tr>";
-                        data.forEach(item => {
-                            htmlContent += `<tr>
-                                <td>${item.date}</td>
-                                <td>R$ ${item.sale_price}</td>
-                                <td>${item.hat_name}</td>
-                                <td>${item.hat_description}</td>
-                            </tr>`;
-                        });
-                        htmlContent += "</table>";
-                        document.getElementById("user-data").innerHTML = htmlContent;
-                    } else {
-                        document.getElementById("user-data").innerHTML = "Nenhuma informação encontrada.";
-                    }
-                }
-            })
-            .catch(error => {
-                document.getElementById("user-data").innerHTML = `Erro ao buscar dados: ${error.message}`;
-                console.error("Erro ao buscar dados:", error);
-            });
-    } else {
-        document.getElementById("user-data").innerHTML = "ID do usuário não fornecido.";
-    }
-});
+function identificaUsuario() {
+    let urlParams2 = new URLSearchParams(window.location.search);
+    fetch('../bibliotecas/get_user_data.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            'id': urlParams2.get('user')
+        })
+    })
+    .then(response => response.json()) // Converter a resposta para JSON
+    .then(data => {
+        console.log(data)
+        document.getElementsByClassName("title")[0].innerText = "Seja bem-vindo, " + data.name;
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => identificaUsuario());
