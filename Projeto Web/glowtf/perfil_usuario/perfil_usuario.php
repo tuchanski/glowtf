25d9a7e03eba9da92a2f11d2aa02165a197f21c5
Tuchanski <gui.rocha2004@gmail.com>
2024-06-12 13:17:33 -0300

    Atualiza PHP, Js e HTML condizente com outras pastas.

--- Projeto Web/glowtf/perfil_usuario/perfil_usuario.php
+++ Projeto Web/glowtf/perfil_usuario/perfil_usuario.php
@@ -1,33 +1,47 @@
 <?php
 $host = "localhost";
-$port = "3310"; 
+$port = "3310";
 $usuario = "root";
-$senha = ""; 
+$senha = "";
 $banco = "glowtfdb";
 
-$conn = new mysqli($host, $usuario, $senha, $banco);
+$conn = new mysqli($host, $usuario, $senha, $banco, $port);
 
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
-}
+} else {
+    // $sql = "SELECT
+    //             hat.id AS hat_id,
+    //             hat.name AS hat_name,
+    //             hat.inventory,
+    //             hat.price,
+    //             hat.promo_image AS hat_promo_image,
+    //             hat.description,
+    //             hat.wiki,
+    //             paint.paint_id,
+    //             paint.name AS paint_name,
+    //             paint.promo_image AS paint_promo_image,
+    //             paint.hex_color
+    //         FROM
+    //             hat
+    //         LEFT JOIN
+    //             paint ON hat.paint_id = paint.paint_id
+    //         WHERE
+    //             hat.inventory > 0;";
 
-if (isset($_GET['id_user'])) {
-    $user_id = $_GET['id_user'];
+    
+    $sql = "SELECT sale.id as sale_id, sale.date as sale_date, sale.id_user, sale.id_coupon, sale.price as sale_price,
+    sale_has_hat.id as sale_has_hat_id, sale_has_hat.id_hat, sale_has_hat.price as hat_price,
+    hat.name as hat_name, hat.description as hat_description, hat.promo_image as hat_promo_image,
+    paint.name as paint_name, paint.promo_image as paint_promo_image
+    FROM sale 
+    LEFT JOIN sale_has_hat ON sale_has_hat.id_sale = sale.id
+    LEFT JOIN hat ON hat.id = sale_has_hat.id_hat
+    LEFT JOIN paint ON paint.paint_id = hat.paint_id
+    WHERE sale.id_user = 2";
 
-    $sql = "SELECT sale.id as sale_id, sale.date, sale.id_user, sale.id_coupon, sale.price as sale_price,
-                   sale_has_hat.id as sale_has_hat_id, sale_has_hat.id_hat, sale_has_hat.price as hat_price,
-                   hat.name as hat_name, hat.description as hat_description
-                FROM sale 
-                INNER JOIN sale_has_hat ON sale_has_hat.id_sale = sale.id
-                INNER JOIN hat ON hat.id = sale_has_hat.id_hat
-                WHERE sale.id_user = ?";
-    
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $user_id);
-    $stmt->execute();
-    
-    $result = $stmt->get_result();
-    
+    $result= $conn->query($sql);
+   
     if ($result->num_rows > 0) {
         $rows = array();
         while($row = $result->fetch_assoc()) {
@@ -38,9 +52,6 @@ if (isset($_GET['id_user'])) {
     } else {
         echo json_encode([]);
     }
-    $stmt->close();
-} else {
-    echo json_encode(["error" => "ID do usuário não fornecido"]);
 }
 
 $conn->close();
