07f36804e9cf325e43a9857334720fd770988368
Tuchanski <gui.rocha2004@gmail.com>
2024-06-13 16:18:35 -0300

    Cria carrinho e wishlist pro usuario novo.

--- Projeto Web/glowtf/cadastro/teste_login.php
+++ Projeto Web/glowtf/cadastro/teste_login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if(isset($_POST['submit']) && !empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['cpf']) && !empty($_POST['genero']) && !empty($_POST['estado']) && !empty($_POST['senha']) && !empty($_POST['steam']) && !empty($_POST['confirma-senha'])) {
+if (isset($_POST['submit']) && !empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['cpf']) && !empty($_POST['genero']) && !empty($_POST['estado']) && !empty($_POST['senha']) && !empty($_POST['steam']) && !empty($_POST['confirma-senha'])) {
     include_once('config.php');
     
     $nome = mysqli_real_escape_string($conn, $_POST['nome']);
@@ -46,13 +46,27 @@ if(isset($_POST['submit']) && !empty($_POST['nome']) && !empty($_POST['email']) 
     $sql = "SELECT * FROM user WHERE email = '$email' OR cpf = '$cpf'";
     $result = $conn->query($sql);
 
-    if(mysqli_num_rows($result) < 1) {
+    if (mysqli_num_rows($result) < 1) {
         $adicionaUsuario = mysqli_query($conn, "INSERT INTO user (name, gender, state, cpf, email, password, admin, id_steam) VALUES ('$nome', '$genero', '$estado', '$cpf', '$email', '$senha', 0, '$steam')");
-        $_SESSION['cadastro_status'] = 'sucesso'; 
-        header('Location: ../login/login.html?cadastro_status=sucesso');
+        
+        if ($adicionaUsuario) {
+            $idUsuario = mysqli_insert_id($conn);
+
+            $dataAtual = date('Y-m-d H:i:s');
+            $adicionaCarrinho = mysqli_query($conn, "INSERT INTO cart (id_user, date) VALUES ('$idUsuario', '$dataAtual')");
+
+            $adicionaWishlist = mysqli_query($conn, "INSERT INTO wishlist (id_user) VALUES ('$idUsuario')");
+
+            $_SESSION['cadastro_status'] = 'sucesso';
+            header('Location: ../login/login.html?cadastro_status=sucesso');
+        } else {
+            $_SESSION['cadastro_status'] = 'erro_insercao';
+            header('Location: cadastro.html?cadastro_status=erro_insercao');
+        }
+
         exit();
     } else {
-        $_SESSION['cadastro_status'] = 'erro'; 
+        $_SESSION['cadastro_status'] = 'erro';
         header('Location: cadastro.html?cadastro_status=erro');
         exit();
     }
