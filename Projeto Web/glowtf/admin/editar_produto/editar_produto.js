5c3b672a04bb26f781354459ad0344cd02975796
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-06-12 22:35:17 -0300

    Adiciona options de tinta

--- Projeto Web/glowtf/admin/editar_produto/editar_produto.js
+++ Projeto Web/glowtf/admin/editar_produto/editar_produto.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const inputUploadImagem = document.getElementById('upload-imagem');
-  const divImagens = document.querySelector('.imagens');
-  const imgProduto = document.querySelector('.imagem-produto');
+  // const inputUploadImagem = document.getElementById('upload-imagem');
+  // const divImagens = document.querySelector('.imagens');
+  // const imgProduto = document.querySelector('.imagem-produto');
   const selectTinta = document.getElementById('tinta');
   const imgSplash = document.querySelector('.splash');
 
@@ -44,22 +44,22 @@ function cadastraProduto() {
     method: 'POST',
     body: formData
   })
-  .then(response => response.text())
-  .then(data => {
+    .then(response => response.text())
+    .then(data => {
       console.log(data);
       // alert('Produto adicionado com sucesso!');
       // MoverPagina('../lista_de_produtos.html');
-  })
-  .catch(error => {
+    })
+    .catch(error => {
       console.error('Erro:', error);
       alert('Ocorreu um erro ao enviar o formulário.');
-  });
+    });
 }
 
 
 function carregaClasses() {
   const listaClasses = document.getElementsByClassName("classe")[0];
-    
+
   fetch("classes.php")
     .then((response) => {
       if (!response.ok) {
@@ -73,12 +73,12 @@ function carregaClasses() {
         listaClasses.insertAdjacentHTML("beforeend", classHtml);
       });
     })
-  .catch((error) => console.error("Error:", error));
+    .catch((error) => console.error("Error:", error));
 }
 
 function carregaTintas() {
   const listaTintas = document.getElementsByClassName("tinta")[0];
-    
+
   fetch("tintas.php")
     .then((response) => {
       if (!response.ok) {
@@ -100,95 +100,106 @@ function carregaTintas() {
 const mostraProduto = document.getElementsByClassName("fundo-adiciona-produto")[0];
 
 function carregaProduto() {
-  fetch("produto.php")
+  const produtoPromise = fetch("produto.php")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Erro na conexão");
       }
       return response.json();
     })
-    .then((data) => {
-      console.log("dados: ", data);
 
-      let produto = `
-      <div class="dados-produto">
-        <form id="formProduto" class="login-layout">
-          <div class="input-produto">
-            <div>
-              <label for="nome-produto">Nome:</label>
-            </div>
-            <div>
-              <input type="text" value="${data.hat_name}" id="nome-produto" name="nome-produto" placeholder="Digite seu nome do produto" required>
-            </div>
-          </div>
-          <div class="input-produto">
-            <div>
-              <label for="preco-produto">Preço:</label>
-            </div>
-            <div>
-              <input type="number" value="${(data.price/100).toFixed(2)}" id="preco-produto" name="preco-produto" placeholder="Digite o preço do produto" required>
-            </div>
-          </div>
-          <div class="input-produto">
-            <div>
-              <label for="estoque-produto">Estoque:</label>
-            </div>
-            <div>
-              <input type="text" value="${data.inventory}" id="estoque" name="estoque" placeholder="Insira o estoque do produto" required>
-            </div>
-          </div>
-          <div class="input-produto">
-            <div>
-              <label for="descricao">Descrição:</label>
-            </div>
-            <div>
-              <input type="text" value="${data.description}" id="descricao" name="descricao" placeholder="Insira a descrição do produto" required>
-            </div>
-          </div>
-          <div class="input-produto">
-            <div>
-              <label for="wiki-produto">Wiki:</label>
-            </div>
-            <div>
-              <input type="text" value="${data.wiki}" id="wiki-produto" name="wiki-produto" placeholder="Insira o link da wiki do produto" required>
-            </div>
-          </div>
-          <div class="input-imagem">
-            <label for="imagem">Selecione a imagem do chapéu:</label>
-            <input type="file" value="${data.hat_promo_image}" name="upload-imagem" class="upload-imagem" id="upload-imagem">
-          </div>
-          <div class="input-produto">
-            <div>
-              <label for="tinta">Tinta:</label><br>
-            </div>
-            <div>
-              <select class="tinta" name="tinta" id="tinta">
-              </select>
-            </div>
-          </div>
-          <div class="input-produto">
-            <div>
-              <label for="classe">Selecione a classe do chapéu (Ctrl + click para selecionar múltiplas): </label><br>
-            </div>
-            <div>
-              <select class="classe" name="classe" multiple id="classe">
-              </select>
-            </div>
-          </div>
-          <div class="imagem-botao">
-            <div class="imagens">
-              <img class="imagem-produto" src="../../dados/imagens/itens_do_jogo/${data.hat_promo_image
-              }">
-                <img class="splash" src="../../dados/imagens/tintas/${data.paint_promo_image
-              }">
-            </div>
-          </div>
-        </form>
-        <button class="button btn-input" type="button" action="./index.html" onclick="cadastraProduto()">Cadastrar produto</button>
-      </div>`;
-
-      mostraProduto.insertAdjacentHTML("beforeend", produto);
+  const tintasPromise = fetch("tintas.php")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro na conexão");
+      }
+      return response.json();
     })
+
+  Promise.all([produtoPromise, tintasPromise])
+    .then(([produto, tintas]) => {
+    console.log("dados: ", produto);
+
+    let tintasOptions = "<option value=\"\">Nenhuma</option>\n"
+    tintasOptions += tintas.map((tinta) => `<option value='${tinta.paint_id}'>${tinta.name}</option>`).join("\n")
+    const produtoHtml = `
+    <div class="dados-produto">
+      <form id="formProduto" class="login-layout">
+        <div class="input-produto">
+          <div>
+            <label for="nome-produto">Nome:</label>
+          </div>
+          <div>
+            <input type="text" value="${produto.hat_name}" id="nome-produto" name="nome-produto" placeholder="Digite seu nome do produto" required>
+          </div>
+        </div>
+        <div class="input-produto">
+          <div>
+            <label for="preco-produto">Preço:</label>
+          </div>
+          <div>
+            <input type="number" value="${(produto.price / 100).toFixed(2)}" id="preco-produto" name="preco-produto" placeholder="Digite o preço do produto" required>
+          </div>
+        </div>
+        <div class="input-produto">
+          <div>
+            <label for="estoque-produto">Estoque:</label>
+          </div>
+          <div>
+            <input type="text" value="${produto.inventory}" id="estoque" name="estoque" placeholder="Insira o estoque do produto" required>
+          </div>
+        </div>
+        <div class="input-produto">
+          <div>
+            <label for="descricao">Descrição:</label>
+          </div>
+          <div>
+            <input type="text" value="${produto.description}" id="descricao" name="descricao" placeholder="Insira a descrição do produto" required>
+          </div>
+        </div>
+        <div class="input-produto">
+          <div>
+            <label for="wiki-produto">Wiki:</label>
+          </div>
+          <div>
+            <input type="text" value="${produto.wiki}" id="wiki-produto" name="wiki-produto" placeholder="Insira o link da wiki do produto" required>
+          </div>
+        </div>
+        <div class="input-imagem">
+          <label for="imagem">Selecione a imagem do chapéu:</label>
+          <input type="file" value="${produto.hat_promo_image}" name="upload-imagem" class="upload-imagem" id="upload-imagem">
+        </div>
+        <div class="input-produto">
+          <div>
+            <label for="tinta">Tinta:</label><br>
+          </div>
+          <div>
+            <select class="tinta" name="tinta" id="tinta">
+            ${tintasOptions}
+            </select>
+          </div>
+        </div>
+        <div class="input-produto">
+          <div>
+            <label for="classe">Selecione a classe do chapéu (Ctrl + click para selecionar múltiplas): </label><br>
+          </div>
+          <div>
+            <select class="classe" name="classe" multiple id="classe">
+            </select>
+          </div>
+        </div>
+        <div class="imagem-botao">
+          <div class="imagens">
+            <img class="imagem-produto" src="../../dados/imagens/itens_do_jogo/${produto.hat_promo_image}">
+              <img class="splash" src="../../dados/imagens/tintas/${produto.paint_promo_image}">
+          </div>
+        </div>
+      </form>
+      <button class="button btn-input" type="button" action="./index.html" onclick="cadastraProduto()">Cadastrar produto</button>
+    </div>`;
+
+    mostraProduto.insertAdjacentHTML("beforeend", produtoHtml);
+  })
   .catch((error) => console.error("Error:", error));
 }
 
--- Projeto Web/glowtf/admin/editar_produto/tintas.php
+++ Projeto Web/glowtf/admin/editar_produto/tintas.php
@@ -26,6 +26,7 @@ if ($conn->connect_error) {
             $rows[] = $row;
         }
         header('Content-Type: application/json');
+        sleep(3);
         echo json_encode($rows);
     } else {
         echo json_encode([]);
