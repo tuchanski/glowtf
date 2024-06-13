1c9c49cdd6b8410ae923408139b80144ef351b48
Tuchanski <gui.rocha2004@gmail.com>
2024-06-12 23:11:38 -0300

    Adicionado deletar itens e conexões com banco no geral.

---
+++ Projeto Web/glowtf/lista_de_desejos/lista_de_desejos.js
@@ -0,0 +1,116 @@
+function carregaProdutos() {
+    let urlParams2 = new URLSearchParams(window.location.search);
+    const listaProdutos = document.getElementsByClassName("itens")[0];
+    fetch("lista_de_desejos.php", {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+          'id': urlParams2.get('user')
+      })
+  })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erro na conexão");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        data.forEach((data) => {
+          const item = `
+          <tr>
+            <td class="imagens">
+                <img class="imagem" src="../dados/imagens/itens_do_jogo/${data.hat_promo_image}">
+                <img class="splash" src="../dados/imagens/tintas/${data.paint_promo_image}">
+            </td>
+            <td>
+              <div class="dados-produto">
+                <div class="titulo">${data.hat_name}</div>
+                  <div class="tinta">
+                    <div class="cor-da-tinta" style="background-color: #${data.hex_color};"></div>
+                    <div class="nome-tinta">${data.paint_name}</div>
+                  </div>
+              </div>
+            </td>
+            <td class="preco">
+                R$ ${data.hat_price}
+            </td>
+            <td class="botoes">
+              <button class="carrinho-btn" type="button" onclick="MoverPagina('../carrinho/carrinho.html', 'id_hat', ${data.hat_id})">
+                <span class="material-symbols-outlined">
+                  shopping_cart
+                </span>
+              </button>
+            </td>
+            <td>
+              <button class="remover-carrinho-btn" type="button" onclick="deletaProduto(${data.wishlist_has_hat_id})">
+                <span class="material-symbols-outlined">
+                  delete
+                </span>
+              </button>
+            </td>
+          </tr>
+          `
+          listaProdutos.insertAdjacentHTML("beforeend", item);
+        });
+      })
+      .catch((error) => console.error("Error:", error));
+  }
+  document.addEventListener("DOMContentLoaded", () => carregaProdutos(""));
+  
+  function identificaUsuario() {
+      let urlParams2 = new URLSearchParams(window.location.search);
+      fetch('../bibliotecas/get_user_data.php', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: new URLSearchParams({
+              'id': urlParams2.get('user')
+          })
+      })
+      .then(response => response.json()) // Converter a resposta para JSON
+      .then(data => {
+        console.log(data)
+          document.getElementsByClassName("title")[0].innerText = "Sua lista de desejos, " + data[0].name;
+      })
+      .catch(error => {
+          console.error('Erro:', error);
+      });
+  }
+  document.addEventListener("DOMContentLoaded", () => identificaUsuario());
+
+
+  function deletaProduto(wishlist_has_hat_id) {
+    let url = 'deleta_item_lista_de_desejos.php';
+    
+    let options = {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded' 
+        },
+        body: 'wishlist_has_hat_id=' + encodeURIComponent(wishlist_has_hat_id) 
+    };
+  
+    fetch(url, options)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao deletar produto');
+        }
+        return response.text();
+    })
+    .then(data => {
+        console.log(data); 
+        
+        alert('Produto deletado com sucesso.');
+  
+        window.location.reload();
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        alert('Erro ao deletar produto.');
+    });
+  }
+  
--- Projeto Web/glowtf/lista_de_desejos/lista_de_desejos.php
+++ Projeto Web/glowtf/lista_de_desejos/lista_de_desejos.php
@@ -1,69 +1,43 @@
 <?php
-// Configurações do banco de dados
-$host = "localhost:3307";
-$usuario = "root"; 
+$host = "localhost";
+
+$port = "3307";
+$usuario = "root";
 $senha = "";
-$banco = "bd_ex_cliente_servidor";
+$banco = "glowtfdb";
 
-// Conexão com o banco de dados
-$conn = new mysqli($host, $usuario, $senha, $banco);
+$conn = new mysqli($host, $usuario, $senha, $banco, $port);
 
-// Verifica a conexão
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
-}
-
-// Obtém os dados do formulário
-$name = $_POST['nome'];
-$lastname = $_POST['sobrenome'];
-$pass = $_POST['senha'];
-$email = $_POST['email'];
-$birthday_day = $_POST['dia'];
-$birthday_month = $_POST['mes'];
-$birthday_year = $_POST['ano'];
-
-if ($name != True) {
-    echo "Por favor, insira seu nome.\n";
-    return;
-}
-
-if ($lastname != True) {
-    echo "Por favor, insira seu sobrenome.\n";
-    return;
-}
-
-if ($pass != True) {
-    echo "Por favor, insira sua senha.\n";
-    return;
-}
-
-if ($email != True) {
-    echo "Por favor, insira seu email.\n";
-    return;
-}
-
-if (2024 - $birthday_year <= 18) {
-    echo "Usuário deve ter mais que 18 anos.\n";
-    return;
-}
-
-if (strlen($pass) <= 10) {
-    echo "Senha fraca, por favor insira 10 caracteres no mínimo.\n";
-    return;
-}
-
-// Insere os dados no banco de dados
-$sql = "INSERT INTO usuarios (nome, senha) VALUES ('$name', '$pass')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Dados inseridos com sucesso!\n";
-
 } else {
-    echo "Erro ao inserir dados: " . $conn->error;
+
+
+    $user = $_POST["id"];
     
+    $sql = "SELECT wishlist_has_hat.id as wishlist_has_hat_id, wishlist.id_user as id_user, hat.price as hat_price,
+    hat.name as hat_name, hat.description as hat_description, hat.promo_image as hat_promo_image, hat.id as hat_id,
+    paint.name as paint_name, paint.promo_image as paint_promo_image, paint.hex_color
+    FROM wishlist_has_hat
+    LEFT JOIN wishlist ON wishlist_has_hat.id_wishlist = wishlist.id_user
+    LEFT JOIN hat ON hat.id = wishlist_has_hat.id_hat
+    LEFT JOIN paint ON paint.paint_id = hat.paint_id
+    WHERE id_user = $user";
+
+
+    $result= $conn->query($sql);
+   
+    if ($result->num_rows > 0) {
+        $rows = array();
+        while($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+        header('Content-Type: application/json');
+        echo json_encode($rows);
+    } else {
+        echo json_encode([]);
+    }
 }
 
-// Fecha a conexão
 $conn->close();
 ?>
- 
