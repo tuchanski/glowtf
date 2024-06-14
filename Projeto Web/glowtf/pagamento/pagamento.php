b4eeabc4ff187af23fe24f954d8b4e60270be0a9
Tuchanski <gui.rocha2004@gmail.com>
2024-06-14 00:39:57 -0300

    Implementa pagamento

--- Projeto Web/glowtf/pagamento/pagamento.php
+++ Projeto Web/glowtf/pagamento/pagamento.php
@@ -1,69 +1,42 @@
 <?php
-// Configurações do banco de dados
-$host = "localhost:3307";
-$usuario = "root"; 
-$senha = "";
-$banco = "bd_ex_cliente_servidor";
+$host = "localhost";
+$port = "3307";
+$usuario = "root";
+$senha = ""; 
+$banco = "glowtfdb";
 
-// Conexão com o banco de dados
-$conn = new mysqli($host, $usuario, $senha, $banco);
+$conn = new mysqli($host, $usuario, $senha, $banco, $port);
 
-// Verifica a conexão
+$cart_has_hat_id = $_GET['cart_has_hat_id'];
+
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
-}
-
-// Obtém os dados do formulário
-$name = $_POST['nome'];
-$lastname = $_POST['sobrenome'];
-$pass = $_POST['senha'];
-$email = $_POST['email'];
-$birthday_day = $_POST['dia'];
-$birthday_month = $_POST['mes'];
-$birthday_year = $_POST['ano'];
-
-if ($name != True) {
-    echo "Por favor, insira seu nome.\n";
-    return;
-}
-
-if ($lastname != True) {
-    echo "Por favor, insira seu sobrenome.\n";
-    return;
-}
-
-if ($pass != True) {
-    echo "Por favor, insira sua senha.\n";
-    return;
-}
-
-if ($email != True) {
-    echo "Por favor, insira seu email.\n";
-    return;
-}
-
-if (2024 - $birthday_year <= 18) {
-    echo "Usuário deve ter mais que 18 anos.\n";
-    return;
-}
-
-if (strlen($pass) <= 10) {
-    echo "Senha fraca, por favor insira 10 caracteres no mínimo.\n";
-    return;
-}
-
-// Insere os dados no banco de dados
-$sql = "INSERT INTO usuarios (nome, senha) VALUES ('$name', '$pass')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Dados inseridos com sucesso!\n";
-
 } else {
-    echo "Erro ao inserir dados: " . $conn->error;
-    
+    $sql = "SELECT cart_has_hat.id AS cart_has_hat_id,
+    cart_has_hat.id_cart AS cart_id,
+    hat.id AS hat_id, hat.inventory AS hat_inventory,
+    hat.price AS hat_price, hat.promo_image AS hat_promo_image,
+    hat.name AS hat_name, hat.description AS hat_description,
+    paint.paint_id AS paint_id, paint.name AS paint_name, 
+    paint.promo_image AS paint_promo_image, paint.hex_color AS hex_color
+    FROM cart_has_hat
+    LEFT JOIN hat ON cart_has_hat.id_hat = hat.id
+    LEFT JOIN paint ON hat.paint_id = paint.paint_id
+    WHERE cart_has_hat.id = $cart_has_hat_id;";
+
+    $result = $conn->query($sql);
+   
+    if ($result->num_rows > 0) {
+        $rows = array();
+        while($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+        header('Content-Type: application/json');
+        echo json_encode($rows);
+    } else {
+        echo json_encode([]);
+    }
 }
 
-// Fecha a conexão
 $conn->close();
 ?>
- 
