a03ca8e6b60fcb8be16b109b2594f2af3b0c88e3
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-06-14 08:42:29 -0300

    Ajuste dropdown hamburger

--- Projeto Web/glowtf/pagamento/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host = "localhost";
-    $port = "3307";
-    $usuario = "root";
-    $senha = "";
-    $banco = "glowtfdb";
-    $conn = new mysqli($host, $usuario, $senha, $banco, $port);
-?>
