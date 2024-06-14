bf8c567f0c3d4dc72af923f85badbb6354b372e8
GiovanniStrasser <giostrasser@gmail.com>
2024-06-13 22:34:43 -0300

    Implementa botão de ver no carrinho na home

--- Projeto Web/glowtf/home/home.js
+++ Projeto Web/glowtf/home/home.js
@@ -1,45 +1,45 @@
 const listaProdutos = document.getElementsByClassName('corpo')[0];
 
 function toggleWishlist(element, current, hat) {
-    console.log("current: " +  current + " | " + hat);
-    let urlParams = new URLSearchParams(window.location.search);
+  console.log("current: " + current + " | " + hat);
+  let urlParams = new URLSearchParams(window.location.search);
 
-    if (urlParams.has("user")) {
-        let userId = urlParams.get("user");
+  if (urlParams.has("user")) {
+    let userId = urlParams.get("user");
 
-        // Update the star color based on the new status
-        let starColor = !current? 'white' : '#282828';
-        element.style.color = starColor;
+    // Update the star color based on the new status
+    let starColor = !current ? 'white' : '#282828';
+    element.style.color = starColor;
 
-        // Update the wishlist status on the server
-        fetch('wishlist.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: new URLSearchParams({
-                'user_id': userId,
-                'hat_id': hat,
-                'action': !current? 'a' : 'r' // Include an action parameter to indicate add/remove
-            })
-        })
-        .then(response => response.text())
-        .then(data => {
-            console.log(data); // Log response from wishlist.php if needed
-        })
-        .catch(error => {
-            console.error('Error toggling wishlist status:', error);
-            // Handle error as needed
-        });
+    // Update the wishlist status on the server
+    fetch('wishlist.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        'user_id': userId,
+        'hat_id': hat,
+        'action': !current ? 'a' : 'r' // Include an action parameter to indicate add/remove
+      })
+    })
+      .then(response => response.text())
+      .then(data => {
+        console.log(data); // Log response from wishlist.php if needed
+      })
+      .catch(error => {
+        console.error('Error toggling wishlist status:', error);
+        // Handle error as needed
+      });
 
-        // Update the onclick function to toggle back to the opposite of newStatus
-        element.onclick = function () {
-            toggleWishlist(element, !current, hat);
-        };
-    } else {
-        window.location.href = '../login/login.html';
-        alert("Você precisa estar logado para ter uma lista de desejos.");
-    }
+    // Update the onclick function to toggle back to the opposite of newStatus
+    element.onclick = function () {
+      toggleWishlist(element, !current, hat);
+    };
+  } else {
+    window.location.href = '../login/login.html';
+    alert("Você precisa estar logado para ter uma lista de desejos.");
+  }
 }
 
 function carregarProdutos(query) {
@@ -49,46 +49,61 @@ function carregarProdutos(query) {
   let cartItems = [];
 
   if (urlParams.has("user")) {
-      // Fetch wishlist and cart items
-      Promise.all([
-          getWishlist(urlParams.get("user")),
-          getCart(urlParams.get("user"))
-      ])
+    // Fetch wishlist and cart items
+    Promise.all([
+      getWishlist(urlParams.get("user")),
+      getCart(urlParams.get("user"))
+    ])
       .then(([wishlistIds, cartIds]) => {
-          wishlistItems = wishlistIds;
-          cartItems = cartIds;
+        wishlistItems = wishlistIds;
+        cartItems = cartIds;
 
-          fetch("home.php")
-              .then((response) => {
-                  if (!response.ok) {
-                      throw new Error("Erro na conexão");
-                  }
-                  return response.json();
-              })
-              .then((data) => {
-                  data.forEach((item) => {
-                      let inWishlist = wishlistItems.includes(String(item.hat_id));
-                      let inCart = cartItems.includes(String(item.hat_id));
+        fetch("home.php")
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Erro na conexão");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            data.forEach((item) => {
+              let inWishlist = wishlistItems.includes(String(item.hat_id));
+              let inCart = cartItems.includes(String(item.hat_id));
 
-                      // Adjust star and cart icon colors based on status
-                      let starColor = inWishlist ? 'white' : '#282828';
-                      let cartIconColor = inCart ? '#ff6347' : '#282828';
+              // Adjust star and cart icon colors based on status
+              let starColor = inWishlist ? 'white' : '#282828';
+              let botaoHTML = inCart ?
+                `
+                <button class="remover-carrinho-btn" type="button" onclick="MoverPagina('../carrinho/carrinho.html')">
+        <span class="material-symbols-outlined">
+            delete
+        </span>
+        <div>Ver no Carrinho</div>
+    </button>
+`
+                :
+                `
+    <button class="carrinho-btn" type="button" onclick="adicionaProduto(${item.hat_id})">
+        <span class="material-symbols-outlined">add_shopping_cart</span>
+        <div>Adicionar ao carrinho</div>
+    </button>
+`
+                ;
 
-                      let colorHex = "";
-                      let colorSplash = "";
+              let colorHex = "";
+              let colorSplash = "";
 
-                      if (item.hex_color != undefined) {
-                          colorHex = `
+              if (item.hex_color != undefined) {
+                colorHex = `
                               <div class="cor-da-tinta" style="background-color: ${item.hex_color};"></div>
                               <div class="card-nome-tinta">${item.paint_name}</div>
                           `;
-                          colorSplash = `<img class="card-splash" src="../dados/imagens/tintas/${item.paint_promo_image}" />`;
-                      }
+                colorSplash = `<img class="card-splash" src="../dados/imagens/tintas/${item.paint_promo_image}" />`;
+              }
 
-                      let card = `
+              let card = `
                           <div class="card">
                               <span class="material-symbols-outlined estrela" style="color: ${starColor}" onclick="toggleWishlist(this, ${inWishlist}, ${item.hat_id})">star</span>
-                              <span class="material-symbols-outlined carrinho" style="color: ${cartIconColor}" onclick="toggleCart(this, ${inCart}, ${item.hat_id})">shopping_cart</span>
                               <div class="card-titulo">${item.hat_name}</div>
                               <div class="card-tinta">
                                   ${colorHex}
@@ -99,108 +114,107 @@ function carregarProdutos(query) {
                               </a>
                               <div class="preco-botao">
                                   <div class="card-preco">R$ ${(item.price / 100).toFixed(2).replace('.', ',')}</div>
-                                  <button class="carrinho-btn" type="button" onclick="adicionaProduto(${item.hat_id}, '${urlParams.get("user")}')">
-                                      <span class="material-symbols-outlined">add_shopping_cart</span>
-                                      <div>Adicionar ao carrinho</div>
-                                  </button>
+                                  ${botaoHTML}
                               </div>
                           </div>
                       `;
 
-                      listaProdutos.insertAdjacentHTML("beforeend", card);
-                  });
-              })
-              .catch((error) => console.error("Error:", error));
+              listaProdutos.insertAdjacentHTML("beforeend", card);
+            });
+          })
+          .catch((error) => console.error("Error:", error));
       })
       .catch(error => {
-          console.error('Failed to fetch wishlist or cart:', error);
+        console.error('Failed to fetch wishlist or cart:', error);
       });
   } else {
-      window.location.href = '../login/login.html';
-      alert("Você precisa estar logado para acessar esta página.");
+    window.location.href = '../login/login.html';
+    alert("Você precisa estar logado para acessar esta página.");
   }
 }
 
 
 document.addEventListener("DOMContentLoaded", () => carregarProdutos(""));
 
-function adicionaProduto(hatId, userId) {
-    let urlParams = new URLSearchParams(window.location.search);
-    if (!urlParams.has('user')) {
-        alert("Você precisa estar logado para realizar compras.");
-        window.location.href = '../login/login.html';
-    } else {
-        let url = '../carrinho/adiciona_item_carrinho.php';
-        let params = 'hat_id=' + encodeURIComponent(hatId) + '&id=' + encodeURIComponent(userId);
+function adicionaProduto(hatId) {
+  let urlParams = new URLSearchParams(window.location.search);
+  if (!urlParams.has('user')) {
+    alert("Você precisa estar logado para realizar compras.");
+    window.location.href = '../login/login.html';
+  } else {
+    let userId = urlParams.get("user");
+    let url = '../carrinho/adiciona_item_carrinho.php';
+    let params = 'hat_id=' + encodeURIComponent(hatId) + '&id=' + encodeURIComponent(userId);
 
-        let options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: params
-        };
+    let options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: params
+    };
 
-        fetch(url, options)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Erro ao adicionar produto');
-                }
-                return response.text();
-            })
-            .then(data => {
-                console.log(data);
-                alert('Produto adicionado ao carrinho com sucesso.');
-                MoverPagina('../carrinho/carrinho.html');
-            })
-            .catch(error => {
-                console.error('Erro:', error);
-                alert('Erro ao adicionar produto.');
-            });
-    }
+    fetch(url, options)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao adicionar produto');
+        }
+        return response.text();
+      })
+      .then(data => {
+        console.log(data);
+        alert('Produto adicionado ao carrinho com sucesso.');
+        MoverPagina('../carrinho/carrinho.html');
+      })
+      .catch(error => {
+        console.error('Erro:', error);
+        alert('Erro ao adicionar produto.');
+      });
+  }
 }
 
+
 function getWishlist(userId) {
-    return fetch('get_wishlist.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-            user_id: userId
-        })
+  return fetch('get_wishlist.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      user_id: userId
     })
+  })
     .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json(); // Parse response as JSON
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Parse response as JSON
     })
     .catch(error => {
-        console.error('Error fetching wishlist:', error);
-        throw error; // Re-throw the error to propagate it further if needed
+      console.error('Error fetching wishlist:', error);
+      throw error; // Re-throw the error to propagate it further if needed
     });
 }
 
 
 function getCart(userId) {
   return fetch('get_cart.php', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-          user_id: userId
-      })
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      user_id: userId
+    })
   })
-  .then(response => {
+    .then(response => {
       if (!response.ok) {
-          throw new Error('Network response was not ok');
+        throw new Error('Network response was not ok');
       }
       return response.json(); // Parse response as JSON
-  })
-  .catch(error => {
+    })
+    .catch(error => {
       console.error('Error fetching cart:', error);
       throw error; // Re-throw the error to propagate it further if needed
-  });
+    });
 }
